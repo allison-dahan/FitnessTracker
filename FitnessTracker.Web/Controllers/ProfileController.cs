5a1aa4422d3a629af8857661121b3786d49e63c6
Allison Dahan <allison.dahan@yahoo.ca>
2025-03-23 10:06:33 -0700

    deliverable 3b

--- FitnessTracker.Web/Controllers/ProfileController.cs
+++ FitnessTracker.Web/Controllers/ProfileController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using FitnessTracker.Business.Services;
 using FitnessTracker.Business.DTOs;
+using FitnessTracker.Models;
 
 namespace FitnessTracker.Web.Controllers
 {
@@ -16,41 +17,103 @@ namespace FitnessTracker.Web.Controllers
         // GET: Profile
         public IActionResult Index()
         {
-            var profileDto = _profileService.GetUserProfile();
-            return View(profileDto);
-        }
+            // Convert DTO to Model
+        var profileDto = _profileService.GetUserProfile();
+        var profileModel = new UserProfile
+        {
+            Id = profileDto.Id,
+            FirstName = profileDto.FirstName,
+            LastName = profileDto.LastName,
+            Email = profileDto.Email,
+            Age = profileDto.Age,
+            Gender = profileDto.Gender,
+            Height = profileDto.Height,
+            Weight = profileDto.Weight,
+            FitnessGoal = profileDto.FitnessGoal,
+            TargetWeight = profileDto.TargetWeight
+        };
+
+        return View(profileModel);
+    }
 
         // GET: Profile/Edit
         public IActionResult Edit()
         {
             var profileDto = _profileService.GetUserProfile();
-            return View(profileDto);
+            var profileModel = new UserProfile
+    {
+        Id = profileDto.Id,
+        FirstName = profileDto.FirstName,
+        LastName = profileDto.LastName,
+        Email = profileDto.Email,
+        Age = profileDto.Age,
+        Gender = profileDto.Gender,
+        Height = profileDto.Height,
+        Weight = profileDto.Weight,
+        FitnessGoal = profileDto.FitnessGoal,
+        TargetWeight = profileDto.TargetWeight
+    };
+    return View(profileModel);
         }
 
         // POST: Profile/Edit
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Edit(UserProfileDto profileDto)
+        public IActionResult Edit(UserProfile userProfile)
         {
+            ModelState.Remove(nameof(userProfile.TargetWeight));
             if (ModelState.IsValid)
-            {
-                _profileService.UpdateProfile(profileDto);
-                return RedirectToAction(nameof(Index));
-            }
-            return View(profileDto);
+    {
+        // Convert UserProfile to UserProfileDto
+        var profileDto = new UserProfileDto
+        {
+            Id = userProfile.Id,
+            FirstName = userProfile.FirstName,
+            LastName = userProfile.LastName,
+            Email = userProfile.Email,
+            Age = userProfile.Age,
+            Gender = userProfile.Gender,
+            Height = userProfile.Height,
+            Weight = userProfile.Weight
+        };
+
+        _profileService.UpdateProfile(profileDto);
+        return RedirectToAction(nameof(Index));
+    }
+
+     // If model is not valid, return to Index view
+    return View(nameof(Index), userProfile);
         }
 
         // POST: Profile/UpdateGoals
         [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult UpdateGoals(UserProfileDto profileDto)
+[ValidateAntiForgeryToken]
+public IActionResult UpdateGoals(UserProfile userProfile)
+{
+    // Remove validation for personal info fields
+    ModelState.Remove(nameof(userProfile.FirstName));
+    ModelState.Remove(nameof(userProfile.LastName));
+    ModelState.Remove(nameof(userProfile.Email));
+    ModelState.Remove(nameof(userProfile.Age));
+    ModelState.Remove(nameof(userProfile.Gender));
+    ModelState.Remove(nameof(userProfile.Height));
+    ModelState.Remove(nameof(userProfile.Weight));
+
+    if (ModelState.IsValid)
+    {
+        var profileDto = new UserProfileDto
         {
-            if (ModelState.IsValid)
-            {
-                _profileService.UpdateFitnessGoals(profileDto);
-                return RedirectToAction(nameof(Index));
-            }
-            return View(nameof(Index), profileDto);
-        }
+            Id = userProfile.Id,
+            FitnessGoal = userProfile.FitnessGoal,
+            TargetWeight = userProfile.TargetWeight
+        };
+
+        _profileService.UpdateFitnessGoals(profileDto);
+        return RedirectToAction(nameof(Index));
+    }
+
+    // If model is not valid, return to Index view
+    return View(nameof(Index), userProfile);
+}
     }
 }
--- FitnessTracker.Web/Program.cs
+++ FitnessTracker.Web/Program.cs
@@ -4,32 +4,29 @@ using FitnessTracker.Business.Services;
 using FitnessTracker.DataAccess;
 using Microsoft.EntityFrameworkCore;
 
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container
 builder.Services.AddControllersWithViews();
 
-// Registering the Profile Service
-builder.Services.AddScoped<IProfileService, ProfileService>();
 
-// Registering Repositories (Choose Mock or Database based on environment)
-if (builder.Environment.IsDevelopment())
-{
-    // Use mock repository in development
-    builder.Services.AddScoped<IProfileRepository, MockProfileRepository>();
-}
-else
-{
-    // Use database repository in production
-    builder.Services.AddScoped<IProfileRepository, ProfileDatabaseRepository>();
-}
 
 // Database Context
 builder.Services.AddDbContext<FitnessTrackerContext>(options =>
     options.UseMySql(
         builder.Configuration.GetConnectionString("DefaultConnection"),
-        new MySqlServerVersion(new Version(8, 2, 0))
+        new MySqlServerVersion(new Version(8, 0, 21))
     ));
+
+// Repositories (Database)
+builder.Services.AddScoped<IProfileRepository, ProfileDatabaseRepository>();
+builder.Services.AddScoped<INutritionRepository, NutritionDatabaseRepository>();
+builder.Services.AddScoped<IWorkoutRepository, WorkoutDatabaseRepository>();
+builder.Services.AddScoped<IStatsRepository, StatsDatabaseRepository>();
+
+// Services 
+builder.Services.AddScoped<IProfileService, ProfileService>();
 
 var app = builder.Build();
 
@@ -43,10 +40,17 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
-app.UseAuthorization();
+// app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+// Optional: Ensure database is created
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<FitnessTrackerContext>();
+    context.Database.EnsureCreated();
+}
 
 app.Run();
